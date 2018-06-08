b55d0aca47780c48560a3ab12012cef3f25378f4
tam <tamara.acevedoavila@gmail.com>
2018-06-08 19:38:47 -0400

    agrega grid de 10 columnas bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,11 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'javascripts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
